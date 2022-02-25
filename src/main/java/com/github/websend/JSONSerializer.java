c9eec0800d08ee83108926f9e79c363767da325a
XeonG <xeon.g8@gmail.com>
2022-02-25 21:01:33 +0000

    broken inv stuff 1.18.. commented out

--- src/main/java/com/github/websend/JSONSerializer.java
+++ src/main/java/com/github/websend/JSONSerializer.java
@@ -2,11 +2,11 @@ package com.github.websend;
 
 import com.github.websend.spigot.SpigotJSONSerializer;
 import com.google.gson.Gson;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.minecraft.nbt.MojangsonParser;
-import net.minecraft.nbt.NBTTagCompound;
+//import com.mojang.brigadier.exceptions.CommandSyntaxException;
+//import net.minecraft.nbt.MojangsonParser;
+//import net.minecraft.nbt.NBTTagCompound;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_17_R1.inventory.CraftItemStack;
+//import org.bukkit.craftbukkit.v1_17_R1.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -48,8 +48,8 @@ public abstract class JSONSerializer {
                 player.put("OffHandItemID", ply.getInventory().getItemInOffHand().getType().name());
                 JSONObject location = serializeLocation(ply.getLocation());
                 player.put("Location", location);
-                JSONArray inventory = serializeInventory(ply.getInventory());
-                player.put("Inventory", inventory);
+//                JSONArray inventory = serializeInventory(ply.getInventory());
+//                player.put("Inventory", inventory);
             }
         }
         return player;
@@ -68,56 +68,56 @@ public abstract class JSONSerializer {
         return location;
     }
 
-    public JSONArray serializeInventory(Inventory inv) throws JSONException {
-        JSONArray inventory = new JSONArray(); {
-            for (int i = 0; i < inv.getSize(); i++) {
-                ItemStack itemStack = inv.getItem(i);
-                if (itemStack != null) {
-                    JSONObject item = new JSONObject();
-                    item.put("Slot", i);
-
-                    item.put("TypeName", itemStack.getType().name());
-                    item.put("Amount", itemStack.getAmount());
-
-                    if (itemStack.hasItemMeta()) {
-                        // method to get nbt json from the legacy minecraft code
-                        net.minecraft.world.item.ItemStack CBStack = CraftItemStack.asNMSCopy(itemStack);
-                        NBTTagCompound itemTag = CBStack.getTag();
-                        Gson gson = new Gson();
-                        String json = gson.toJson(itemTag);
-                        item.put("nbt_json", json);
-
-                        // method to get nbt_raw without converting it to json
-                        // this is the current default instead of using the JSON above
-                        String tagString = itemTag.toString();
-
-                        try {
-                            String parsedString = MojangsonParser.parse(tagString).toString();
-                            item.put("nbt_raw", parsedString);
-                        } catch (CommandSyntaxException ex) {
-                            Logger.getLogger(JSONSerializer.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-
-                        // method to get nbt from the spigot API instead of the legacy minecraft code
-                        // the issue here is that enchantments have the wrong name, e.g. "SWEEPING_EDGE" instead of "minecraft:sweeping"
-
-                        Main.logDebugInfo("embedding item nbt_json_api for inventory slot " + i);
-                        Map<String, Object> serialMeta;
-
-                        serialMeta = itemStack.getItemMeta().serialize();
-                        String json2 = serialMeta.toString();
-                        // converting to JSON does not work. this would cause a crash, so we rather let this happen on the PHP side.
-                        // main issue here is how crossbows once they were charged behave.
-                        //
-                        // String json2 = gson2.toJson(testing);
-                        item.put("nbt_json_api_string", json2);
-                    }
-
-                    inventory.put(item);
-                }
-            }
-        }
-        return inventory;
-    }
+//    public JSONArray serializeInventory(Inventory inv) throws JSONException {
+//        JSONArray inventory = new JSONArray(); {
+//            for (int i = 0; i < inv.getSize(); i++) {
+//                ItemStack itemStack = inv.getItem(i);
+//                if (itemStack != null) {
+//                    JSONObject item = new JSONObject();
+//                    item.put("Slot", i);
+//
+//                    item.put("TypeName", itemStack.getType().name());
+//                    item.put("Amount", itemStack.getAmount());
+//
+//                    if (itemStack.hasItemMeta()) {
+//                        // method to get nbt json from the legacy minecraft code
+//                        net.minecraft.world.item.ItemStack CBStack = CraftItemStack.asNMSCopy(itemStack);
+//                        NBTTagCompound itemTag = CBStack.getTag();
+//                        Gson gson = new Gson();
+//                        String json = gson.toJson(itemTag);
+//                        item.put("nbt_json", json);
+//
+//                        // method to get nbt_raw without converting it to json
+//                        // this is the current default instead of using the JSON above
+//                        String tagString = itemTag.toString();
+//
+//                        try {
+//                            String parsedString = MojangsonParser.parse(tagString).toString();
+//                            item.put("nbt_raw", parsedString);
+//                        } catch (CommandSyntaxException ex) {
+//                            Logger.getLogger(JSONSerializer.class.getName()).log(Level.SEVERE, null, ex);
+//                        }
+//
+//                        // method to get nbt from the spigot API instead of the legacy minecraft code
+//                        // the issue here is that enchantments have the wrong name, e.g. "SWEEPING_EDGE" instead of "minecraft:sweeping"
+//
+//                        Main.logDebugInfo("embedding item nbt_json_api for inventory slot " + i);
+//                        Map<String, Object> serialMeta;
+//
+//                        serialMeta = itemStack.getItemMeta().serialize();
+//                        String json2 = serialMeta.toString();
+//                        // converting to JSON does not work. this would cause a crash, so we rather let this happen on the PHP side.
+//                        // main issue here is how crossbows once they were charged behave.
+//                        //
+//                        // String json2 = gson2.toJson(testing);
+//                        item.put("nbt_json_api_string", json2);
+//                    }
+//
+//                    inventory.put(item);
+//                }
+//            }
+//        }
+//        return inventory;
+//    }
 
 }
